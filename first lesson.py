1ba4ee9d3d26ba6f4db1cc5a1532c83d12e3aa1c
metarghi97 <metarghi97@icloud.com>
2024-09-27 15:23:22 +0100

    Add files via upload

---
+++ first lesson.py
@@ -0,0 +1,9 @@
+# first lesson
+some_code = 1123 #this is a comment
+""" so i am just going to waffle and talk about how manchester united is the
+    best team in the world. anyways i think ill leave it there"""
+
+name = "muhammed" #string
+height = 167.5 #floats
+country = 'united kingdom' #string
+no_of_kids = 2 # integer
